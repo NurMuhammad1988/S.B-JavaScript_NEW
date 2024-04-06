d77ad32d2cb33359a2f2019bbb793ad37b4ac24a
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-04-06 18:24:19 +0500

    sss

--- 9.chi dars Amalyot Uyga vazifa/script9.js
+++ 9.chi dars Amalyot Uyga vazifa/script9.js
@@ -1,15 +1,34 @@
 "use strict";
 
- const numberOfSeries = +prompt("Nechta serial ko'rdingiz?" , "")
- console.log(numberOfSeries);
+const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", ""); //+ promptda + belgisi javoblar asosan number malumot turi bo'lganligi sababli qo'yildi
 
+const seriesDB = {
+    count: numberOfSeries, //yani birnchi nechta serial ko'rdingizsavolini javobini shu object ichiga qiymat sifatida berib qo'yildi shunda logga savolga berilgan javob chiqadi yani numberOfSeriesda berilgan savolni javobi shu countni ichiga tushadi // bu +prompt bo'lgani uchun agar javob number malumot turi bo'lmasa NaN chiqadi yani not a number
+    series: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-//  const seriesDB =   {
+const a = prompt("Ohirgi ko'rgan serialingiz?"),
+    b = prompt("Nechchi baho berasiz?"),
+    c = prompt("Ohirgi ko'rgan serialingiz?"),
+    d = prompt("Nechchi baho berasiz?");
 
-//     count:"",
-//     series{},
-//     actors{},
-//     genres[],
-//     privat:false
+seriesDB.series[a] = b; //objectga tashqarida hususiyat berilganda masalan bu holatda [a] shunday to'rtburchak qavus ichida berilishi kerak yani [a] va [c] o'zgaruvchilardagi savollarga javoblar seriesDBni ichidagi series objectiga tushurib olindi va b va d o'zgaruvchilarga tushgan javoblarham seriesDBga tenglashtirib qo'yildi
+seriesDB.series[c] = d; //objectga tashqarida hususiyat berilganda masalan bu holatda [a] shunday to'rtburchak qavus ichida berilishi kerak yani [a] va [c] o'zgaruvchilardagi savollarga javoblar seriesDBni ichidagi series objectiga tushurib olindi va b va d o'zgaruvchilarga tushgan javoblarham seriesDBga tenglashtirib qo'yildi
+//shunda bitta o'zgaruvchini ichiga yana 3 ta jami 4 ta o'zgaruvchi ochildi
 
-//  }
+/////////misol
+const theif = {
+    jacket: "black",
+};
+const property = "age",
+    value = 30;
+theif[property] = value;
+console.log(theif);
+/////////misol
+
+console.log(seriesDB);
+
+////////////////////////////////////////////////////////////////////////////
