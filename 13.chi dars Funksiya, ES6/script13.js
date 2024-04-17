0347768ed6ad6b8d1dc7a46e48a9547b59c5e65a
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-04-17 23:18:03 +0500

    sss

---
+++ 13.chi dars Funksiya, ES6/script13.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// console.log("Open Modal");
+///////////////////////////////////////////////////////////////
+// sayHelloWorld("Hello Junior");//funksiyani yozilgan qatoridan tepadaham chaqirish mumkun
+// function sayHelloWorld(text) {
+//     //bu joyda tex parametr yani funksiya yozilib parametriga kerakli narsa yozilib logda chiqarib funksiyani chaqirganda () <shuni ichiga kerakli narsani masalan textni qo'shib chaqirish mumkun yani funksiyani ichiga kop narsa yozib hohlagan joyda shu funksiyani ichidagi kerakli parametirni alohida chaqirib ishlatish mumkun har safar chaqirib chaqirish parametiriga  hohlagancha kerakli narsa yozish mumkun
+//     console.log(text);
+// }
+// sayHelloWorld("Hello World"); //funcsiyani chaqirish sayHelloWorld() funcsiyani shunday chaqirmasa ishlamaydi ishlashi uchun alohida shunday chaqirish kerak
+// sayHelloWorld("Hello JavaScript");
+// sayHelloWorld("Hello Junior"); //har safar chaqirganda boshqa parametirini chaqirish mumkun yoki har safar chaqirish parametiriga boshqa narsa chaqirish mumkun
+////////////////////////////////////////////////////////////
+// function calc(a, b, c) {
+//     //yani funksiya parametiriga a, byozildi bu degani logda a+b qilinsa bu nima bo'lsaham qo'shib beradi yani qo'shishni iloji bor narsa bo'ldi qo'shib bera oladi masalan funksiyani chaqirish parametirida 10 , b yozilishi a 10 b 5 degani javob esa 15 agar funksiyani chaqirishda string malumot turlari bo'lsaham qo'shib beradi
+//     console.log(a + b + c);
+// }
+// calc("10 ", 5, " salom");
+///////////////////////////////////////////////////////////////
+function calc(a, b) {
+   
+    console.log(a + b);
+}
+calc(10, 5);
+calc(9, 10)
+
+
+
