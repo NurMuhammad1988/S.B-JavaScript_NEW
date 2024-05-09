9d733ce7752a527fb385ba6b97b438b1fa5e6ea6
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-05-10 02:02:41 +0500

    fff

--- 28. chi dars Navigatsiya DOM bo'yicha/script28.js
+++ 28. chi dars Navigatsiya DOM bo'yicha/script28.js
@@ -1,1 +1,23 @@
 "use strict";
+// console.log(document.head);//logga qaralsin
+// console.log(document.body);//logga qaralsin
+// console.log(document.body.childNodes);//logga qaralsin
+// console.log(document.body.firstChild);//logga qaralsin
+// console.log(document.body.firstChild);//logga qaralsin
+// console.log(document.body.lastChild);//logga qaralsin
+// console.log(document.body.firstElementChild);//logga qaralsin
+// console.log(document.body.lastElementChild);//logga qaralsin
+// console.log(document.querySelector("#btn").parentNode);//logga qaralsin
+// console.log(document.querySelector("#btn").parentElement);//logga qaralsin
+// console.log(document.querySelector("#btn").parentNode.parentNode);//logga qaralsin
+// console.log(document.querySelector("[data-current='b']").nextSibling);//logga qaralsin
+// console.log(document.querySelector("[data-current='b']").previousSibling);//logga qaralsin
+// console.log(document.querySelector("[data-current='b']").nextElementSibling);//logga qaralsin
+// console.log(document.querySelector("[data-current='b']").previousElementSibling);//logga qaralsin
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == "#text") {
+        continue;
+    }
+    console.log(node);
+}
