d7700cb1a5885a888c2877182c65306575ca30a2
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-05-12 21:13:29 +0500

    fff

--- 32.chi dars classlist/script32.js
+++ 32.chi dars classlist/script32.js
@@ -1,24 +1,57 @@
-'use strict'
+"use strict";
+const btns = document.querySelectorAll("button"); //yani bu holatda querySelectorAll metodi bilan htmldagi button elementlarini hammasini oldik
+// console.log(btns);//yani querySelectorAll doim nodelistda massiv qaytaradi
+// console.log(btns[0].classList.length);//yani btns o'zgaruvchida  chaqirilgan button elementlarini 1 chisi yani massiv bo'yicha 0 chi elementini classlist qiymatlarini sonini chiqarib berish buyurildi chunki classlist qiymat length qiymat yani massivni qiymatlari yani oddiyroq qilib aytganda metodlari
+// console.log(btns[0].classList.item(0));//item metodi //yani bu holatda btns o'zgaruvchida chaqirilgan buttonlarni 0 chi elementini classlisti yani 0 chi itemi yani elementi chiqarib berdi yani nolinchi classini yani htmldagi blue classini
+// console.log(btns[0].classList.item(1));//item metodi //yani bu holatda btns o'zgaruvchida chaqirilgan buttonlarni 0 chi elementini classlisti yani 1 chi itemi yani elementi chiqarib berdi yani birinchi classini yani htmldagi some classini
 
-const wrapper = document.querySelector('.btn-block'),
-  btns = document.querySelectorAll('button')
 
-btns[0].addEventListener('click', () => {
-  btns[1].classList.toggle('red')
-})
 
-wrapper.addEventListener('click', (event) => {
-  if (event.target && event.target.matches('button.red')) {
-    console.log('btn click')
-  }
-})
 
-// btns.forEach((item) => {
-//   item.addEventListener('click', () => {
-//     console.log('clicked')
-//   })
-// })
 
-const btn = document.createElement('button')
-btn.classList.add('red')
-wrapper.append(btn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////Ustoz bajargani tayyor file///////////////////////////////////
+// const wrapper = document.querySelector(".btn-block"),
+//     btns = document.querySelectorAll("button");
+
+// btns[0].addEventListener("click", () => {
+//     btns[1].classList.toggle("red");
+// });
+
+// wrapper.addEventListener("click", (event) => {
+//     if (event.target && event.target.matches("button.red")) {
+//         console.log("btn click");
+//     }
+// });
+
+// //// btns.forEach((item) => {
+// ////   item.addEventListener('click', () => {
+// ////     console.log('clicked')
+// ////   })
+// //// })
+
+// const btn = document.createElement("button");
+// btn.classList.add("red");
+// wrapper.append(btn);
+//////////////////////////////////////////Ustoz bajargani tayyor file///////////////////////////////////
