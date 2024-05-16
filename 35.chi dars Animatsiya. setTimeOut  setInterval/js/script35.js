365faf5456f9e3710a5185495cd005a8f5fd3e75
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-05-16 06:02:42 +0500

    ddddddd

--- 35.chi dars Animatsiya. setTimeOut  setInterval/js/script35.js
+++ 35.chi dars Animatsiya. setTimeOut  setInterval/js/script35.js
@@ -1,25 +1,43 @@
 "use strict";
 
+// //setTimeout callback funksiyasi
+// //// 1-sintaksis)
+// setTimeout(() => {
+//     //setTimeout callback funksiya hissoblanadi
+//     console.log("set time out 1"); //1000 ms dan keyin set time out texti logda ko'rindi
+// }, 1000);
+
+// //// 2-sintaksis)
+// setTimeout(
+//     (text) => {
+//         //settimeout funksiyasi parametridaham narsa qabul qilib callback funksiyani qavusidan keyin uchinchi parametridaham textni qabul qiladi
+//         console.log(text);
+//     },
+//     2000,
+//     "set time out 2"
+// );
+
+// //// 3-sintaksis)
+// setTimeout(logger,3000)
+// function logger(){
+//   console.log("set time out 3");
+// }
 
 
+////setTimeout va clearInterval funksiyasi
 
+const btn = document.querySelector("#btn");
 
+let timerId;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn.addEventListener("click", () => {
+     timerId = setTimeout(logger, 1000);//bu holatda timerid local o'zgaruvchi hissoblanadi
+});
+clearInterval(timerId);
+function logger() {
+    //lekin real loyihalarda settimeout funksiyasini o'zgaruvchi ichiga yozgan yahshi chunki clearinterval funksiyasi bilan settimeoutni to'htatish kerak bo'ladi
+    console.log("set time out ");
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // const btn = document.querySelector('#btn');
