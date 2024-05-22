56837ab177169f73b5dca497f07567b0d74c3312
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-05-23 01:24:13 +0500

    ffff

--- 38.chi va 39.chi darslar (Web Loyiha-3 va 4 chi darslari) 38.chi dars Loyiha chegara va 39.chi dars Qo'shimcha loyihaga/js/script38.js
+++ 38.chi va 39.chi darslar (Web Loyiha-3 va 4 chi darslari) 38.chi dars Loyiha chegara va 39.chi dars Qo'shimcha loyihaga/js/script38.js
@@ -68,10 +68,8 @@ window.addEventListener("DOMContentLoaded", () => {
     // console.log(Date.parse("2022-08-11"));//run kodega qaralsin//yani 1970 chi yildan buyog'iga yani parse metodini parametridagi dategacha bo'lgan vaqtni millisekundda olib beradi >>1660176000000 millisekund//yani 11 chi avgustgacha bo'lgan vaqtni milliskundda olib beradi
     const deadLine = "2026-12-31";
     function getTimeRemaining(endtime) {
-        let days, hours, minutes, seconds;
-
-        const timer = Date.parse(endtime) - Date.parse(new Date()); //yani bu joyda parse metodi date bilan birga kelgani sabab parametrdagi stringni object holatiga aylantiradi//yani ikkinchi new Dateda hozirgi joriy vaqt yashingan
-
+        let days, hours, minutes, seconds;//bitta let o'zgaruvchi ichida bir nechta o'zgaruvchi yaratish!!! 
+        const timer = Date.parse(endtime) - Date.parse(new Date()); //yani bu joyda parse metodi date bilan birga kelgani sabab parametrdagi stringni object holatiga aylantiradi//yani ikkinchi new Dateda hozirgi joriy vaqt yashiringan//yani bu holatda joriy vaqt bilan 1970 yil oralig'idagi vaqt olindi
         if (timer <= 0) {
             //yani belgilangan muddat tugagandan keyin kun soat daqiqa soniyalar 00 ga aylanadi yani - minusga qarab ketmaydi
             days = 0;
@@ -79,21 +77,19 @@ window.addEventListener("DOMContentLoaded", () => {
             minutes = 0;
             seconds = 0;
         } else {
-            days = Math.floor(timer / (1000 * 60 * 60 * 24)); //yani timer o'zgaruvchidagi 1970 chi yildan hozir shu kiment yozilayotgan kungacha bo'lgan vaqtni millisekunga bo'lib uni 60 ga kopeytirib yani 60 siniyaga ko'peytirib 60 soniyani esa 60 daqiqaga yani soatga ko'peytirib 60 daqiqani esa 24 ga ko'ytridik shunda timer o'zgaruvchidagi 1970 yil 00:00 dan boshlanib shu coment yozilgan vaqtgacha o'tgan millisekundda hissoblanadigan vaqtni soniyaga soatga kunga bo'lib oldik
+            days = Math.floor(timer / (1000 * 60 * 60 * 24)); //yani timer o'zgaruvchidagi 1970 chi yildan hozir shu koment yozilayotgan kungacha bo'lgan vaqtni millisekunga bo'lib uni 60 ga kopeytirib yani 60 s0niyaga ko'peytirib 60 soniyani esa 60 daqiqaga yani soatga ko'peytirib 60 daqiqani esa 24 ga ko'ytridik shunda timer o'zgaruvchidagi 1970 yil 00:00 dan boshlanib shu coment yozilgan vaqtgacha o'tgan millisekundda hissoblanadigan vaqtni soniyaga soatga kunga bo'lib oldik
             hours = Math.floor((timer / (1000 * 60 * 60)) % 24); //yani bu holatda timerni ichidagi vaqtni soatga bo'lib oldik
             minutes = Math.floor((timer / 1000 / 60) % 60); //yani bu holatda timerni ichidagi vaqtni soatdan minutga yani 60 minutga bo'lib oldik
             seconds = Math.floor((timer / 1000) % 60);
         }
-
         return {
-            total,
+            // total,//????????????????????
             days,
             hours,
             minutes,
             seconds,
         }; //return qilingani uchun bu funksiyani boshqa o'zgaruvchigaham chaqirib ishlatish mumkun //yani getTimeRemaining local funksiyasi bizga object qaytaradi
     }
-
     function setClock(selector, endtime) {
         //selector yani tomer o'zgaruvchi ichidagi ona div hissoblanadi
         const timer = document.querySelector(selector), //yani parametrdagi selector ona div bo'lgani uchun ichidagi hamma html elementlarni olish mumkun
@@ -102,9 +98,7 @@ window.addEventListener("DOMContentLoaded", () => {
             minutes = timer.querySelector("#minutes"),
             seconds = timer.querySelector("#seconds"),
             timeInterval = setInterval(updateClock, 1000);
-
         updateClock();
-
         function updateClock() {
             const t = getTimeRemaining(endtime);
             days.innerHTML = getZero(t.days);
@@ -117,7 +111,6 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         }
     }
-
     function getZero(num) {
         if (num >= 0 && num < 10) {
             //agar num katta yokida teng bo'lsa nolga yokida num 10 dan kichkina bo'lsa
@@ -126,8 +119,6 @@ window.addEventListener("DOMContentLoaded", () => {
             return num;
         }
     } //yani getzero funksiyasini updateClock funksiyasiga ulab qo'ydik yani 10 dan kichkina sonlarda  09 08 07 05 04 03 02 01 00 bo'lib chiqadi
-
     setClock(".timer", deadLine); //bu joyda .timer ni nuqta bilan yozilishini sababi htmldagi timer ona classi selector ko'rinishida kelgani uchun jsga classligi aytilmagan edi shu uchun .timer deb jsga bu class deyildi
-
     //38.chi va 39.chi darslar (Web Loyiha-3 va 4 chi darslari) 38.chi dars Loyiha chegara va 39.chi dars Qo'shimcha loyihaga
 });
