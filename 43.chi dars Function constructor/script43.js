7c8110b85c956c060e660c5b6ac109ea7a3ea9a7
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-05-26 04:02:38 +0500

    ddd

--- 43.chi dars Function constructor/script43.js
+++ 43.chi dars Function constructor/script43.js
@@ -3,11 +3,17 @@
 // const number = new Function()//eski yo'li
 // console.dir(number);
 
-function Car(name, color) {
+function Car(name, color, mph) {
     this.name = name;
     this.color = color;
+    this.mph = mph;
     this.isAirbag = true;
+    this.speed = function () {
+        console.log(`Speed of car ${this.name} is ${this.mph}`);
+    };
 }
 
-const bmw = new Car("bmw", "red");
-console.log(bmw);
+const bmw = new Car("bmw", "red", 200);
+const merc = new Car("merc", "black", 300);
+bmw.speed()
+merc.speed()
