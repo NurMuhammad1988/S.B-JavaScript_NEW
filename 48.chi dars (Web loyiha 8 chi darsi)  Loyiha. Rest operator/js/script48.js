2cd50982989f8555ffc6d1bb902f147291d2ff08
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-05-31 06:53:18 +0500

    ddd

--- 48.chi dars (Web loyiha 8 chi darsi)  Loyiha. Rest operator/js/script48.js
+++ 48.chi dars (Web loyiha 8 chi darsi)  Loyiha. Rest operator/js/script48.js
@@ -240,6 +240,12 @@ window.addEventListener("DOMContentLoaded", () => {
     ).render(); //new MenuCard ga parametr sifatida MenuCard classidagi parametrlar chaqirilgan bu parametrlarda esa css qiymatlar mavjud shu ona divlar sabab>>>.menu .container
 
     //MenuCard nomli class yaratib parametrlariga src, alt, title, descr, price, parentSelector nomli qiymatlar berib chiqildi bular htmldagi atributlar yani atrtibutlar bilan html elementlar chaqirildi masalar src=imgni atributi alt=imgni atributi???  descr=descrni titeli va hakozo bu htmldagi filelarni atribut va atributsizham jsga kelishiga sabab this.parent = document.querySelector(parentSelector); shu bilan ona div .menu va .container classlarini olib kelinishi sabab quereselector bilan parentselector qilib ona div chaqirilganda ichidagi elementlar 0 dan boshlab 0,1,2,3 bo'lib massivda keladi shu sabab har bir html element alohida chaqirib o'tirilmadi uchta cardni classlari esa cssda yozib qo'yilgan va shu sabab yangi ochilgan element o'zgaruvchidagi yangi divga innerhtml bilan yozilgani uchun classlarham keldi
-
     //47.chi dars (Web loyiha 7 chi darsi) Class darsi
+
+    //48.chi dars (Web loyiha 8 chi darsi) Loyiha. Rest operator darsi
+
+    
+
+    //48.chi dars (Web loyiha 8 chi darsi) Loyiha. Rest operator darsi
+
 });
