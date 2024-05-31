a59003d27d9a6611904218d0781045ece9ce5b2d
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-06-01 03:39:59 +0500

    ffff

--- 19. chi dars Clonlash, ES6/script19.js
+++ 19. chi dars Clonlash, ES6/script19.js
@@ -11,7 +11,7 @@
 // console.log(y);//yani bu joyda x nomli o'zgaruvchi yaratib unga 10 qiymati berildi va yana y nomli o'zgaruvchi yaratib unga x qiymatini berdik yani o'zidan oldingi qatorda yaratilgan x o'zgaruvchini qiymatini qo'ydik yani x o'zgaruvchidagi 10 qiymatini qo'ydik va y = y qilib unga 5 ni qo'shganimzda y o'zgaruvchini qiymati 15 bo'ldi sabab: y o'zgaruvchidagi x 10 ga teng yani x o'zgaruvchini qiymatini y  o'zgaruvchiga nusxaladik
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// //booleanda copiy qilish yani clonlash
+// ////booleanda copiy qilish yani clonlash
 // let isMarried = false;
 // let isArray = isMarried;//yani isarrayga ismarried copy qilindi
 // isArray = true;
@@ -20,18 +20,18 @@
 // console.log(isArray); //copy sababidan false true bo'ldi yani copy qilingan o'zgaruvchi true qilinidi false o'zgaruvchini esa asil qiymati o'zgarmasdan turipti
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// ////objectda va arrayda muvaffaqiyatsiz, hato copiy qilish yani clonlash
+////objectda va arrayda muvaffaqiyatsiz, hato copiy qilish yani clonlash
 // const number = {
 //     x: 10,
 //     y: 5,
 // };
 // const newNumber = number; //bu joyda number o'zgaruvchini nusxasi olinmadi shunchaki havolasi olindi bu esa hato yani copy qilinmadi yani clonlanmadi
-// newNumber.x = 15;//bu hato pastki qatordagi komentni o'qi
+// newNumber.x = 15; //bu hato pastki qatordagi komentni o'qi
 // console.log(number);
 // console.log(newNumber); //yani bu holatda number o'zgaruvchini newNumber o'zgaruvchiga copy qilmoqchi bo'ldik lekin copymas faqat havolasi yani ssilkasi kelarkan yani number o'zgaruvchini newNumber o'zgaruvchiga copy qilmoqchi bo'lib newNumberni .x qilib 15 qildik lekin logda ikkala o'zgaruvchini qiymatiham o'zgarib ketdi yani 15 ga o'zgarib ketdi yani mewNumber.x=15 o'zini asli bo'lgan number o'zgaruvchiniham qiymatini o'zgartirib yubordi sababi objectda bu usul bilan yani number malumot turidagi yoki boolean malumot turidagi kabi clonlanmaydi yani copy qilinmaydi
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ////objectda va arrayda copiy qilish yani clonlash !!!!!!!!!!BU QIYIN YO'LI!!!!!!!!!!
+////objectda va arrayda copiy qilish yani clonlash !!!!!!!!!!BU QIYIN YO'LI!!!!!!!!!!
 // function nusxaObject(obj) {
 //     //yani bu obj argument yani bu functsia nusxaObject()qilib pastda  chaqirilganda va parametiriga qaysi object yozilsa o'sha objectni intirigatsa qiladi yani tsikilga qo'yib qiymatlarini nusxasini oladi asl nusxaga esa tasir qilmaydi
 //     //yani functsiya yordamida objectdan copy olish
@@ -41,18 +41,17 @@
 //         //for in yani faqat objectlarni tsikl qilish uchun ishlatiladi
 //         objNusxa[key] = obj[key]; //yani bu joydagi holat yani tsikilga let objNusxa ni hususiyatlariga yani keylariga keylari berib chiqildi va nusxaObject ni parametridagi objni keylari berib chiqildi
 //     }
-//     return objNusxa;//qaytarish esa objectni nusxasi hissoblanadi
+//     return objNusxa; //qaytarish esa objectni nusxasi hissoblanadi
 //     //yani umuman aytganda bu joyda nusxaObject nomli functsiya yaratilib uni parametriga obj berib qo'yildi va shu object uchun functsiyani ichida let objNusxa nomli alohi object yaratildi (yani tsiklda nusxalangan objectlar shu yani objNusxa nomli objectga tushadi) va for in tsikli parametriga asosiy nusxaObject  funksiyadagi obj keylari tsikilga qo'yildi va forni ichida objNusxani keylariga obj ni keylari berib chiqildi shunda nusxaObject funksiyasini parametriga qanaqadur object yoki array berilsa object yoki arrayni nusxasini oladi linkini emas nusxasini oladi va bu nusxaObject functsiyani har qanday joyda qayta qayta chaqirib object va arraylarni nusxasini olish mumkun
 // }
 // const numbers = {
 //     x: 10,
 //     y: 5,
 // };
-// const newNumbers = nusxaObject(numbers);//yani numbersdan newnumbersga numbersni qiymatlarini nusxaladik yani nusxasi olinishi kerak bo'lgan object yoki array nusxaObjectni parametrida chaqirilishi kerak
-// newNumbers.x = 15;//yani bu bilan nusxalangan objectni qiymati o'zgartirildi yani numbers objectdagi x ni qiymati 15 ga tenglashtirilib qo'yildi bu holatda numbersda x qiymatni asil qiymati yani 10 o'zgarmadi yani nusxalangan qiymati 15 ga o'zgartirildi
+// const newNumbers = nusxaObject(numbers); //yani numbersdan newnumbersga numbersni qiymatlarini nusxaladik yani nusxasi olinishi kerak bo'lgan object yoki array nusxaObjectni parametrida chaqirilishi kerak
+// newNumbers.x = 15; //yani bu bilan nusxalangan objectni qiymati o'zgartirildi yani numbers objectdagi x ni qiymati 15 ga tenglashtirilib qo'yildi bu holatda numbersda x qiymatni asil qiymati yani 10 o'zgarmadi yani nusxalangan qiymati 15 ga o'zgartirildi
 // console.log(numbers);
-// console.log(newNumbers); //yani nusxaObject funksiyasi yordamida numbers objectini qiymatlarini o'zgartirmasdan yangi newNumbers o'zgaruvchisiga copy qildik yani clonladik
-//BU nusxaObject funksiyasi bilan object array numberlarni clonlash mumkun
+// console.log(newNumbers); //yani nusxaObject funksiyasi yordamida numbers objectini qiymatlarini o'zgartirmasdan yangi newNumbers o'zgaruvchisiga copy qildik yani clonladikBU nusxaObject funksiyasi bilan object array numberlarni clonlash mumkun
 // //bu object va arrayni clonlashni eng qiyin yo'li ekan!!!
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -92,7 +91,7 @@
 //     x: 10,
 //     y: 5,
 // };
-// const newNumbers = Object.assign({}, numbers);//yani object assign metodi ikkita parametr qabul qiladi birinchisi target object yani nusxalangan objectni solib beradi yani {} qo'yilsa nusxalarni bo'sh objectga solib beradi nimani yani ikkinchi manbani yani bu holatda numbers objectni
+// const newNumbers = Object.assign({}, numbers);//yani object assign metodi ikkita parametr qabul qiladi birinchisiga target object yani nusxalangan objectni solib beradi yani {} qo'yilsa nusxalarni bo'sh objectga solib beradi nimani yani ikkinchi manbani yani bu holatda numbers objectni yani assignni ikkinchi parametridagi objectni
 // console.log(numbers);
 // newNumbers.x = 15;
 // console.log(newNumbers); //yani Object.assign() metodi bilan es6 da clonlash juda oson
@@ -113,7 +112,7 @@
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Arraylarda clonlash
+// Arraylarda clonlash slice metodi
 // const arr = [1, 2, 3];
 // const numbers = arr.slice(); //yani arrayni clonlash slice() metodi bilan amalga oshiriladi yani bu joyda numbers nomli yangi o'zgaruvchi ochib shu yangi o'zgaruvchiga arr massivini slice metodi bilan kopiya yani clonlab olindi
 // console.log(arr); //yani asosiy arrayni logi
@@ -141,3 +140,56 @@
 // console.log(liverpool);
 // console.log(manCity);
 // console.log(mixPlayer);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////objectda va arrayda copiy qilish yani clonlash !!!!!!!!!!BU QIYIN YO'LI!!!!!!!!!!
+//commentsiz tushunish uchun yozilgani
+// function nusxaObject(obj) {
+//     let objNusxa = {};
+//     for (let key in obj) {
+//         objNusxa[key] = obj[key];
+//     }
+//     return objNusxa;
+// }
+// const numbers = {
+//     x: 10,
+//     y: 5,
+// };
+// const newNumbers = nusxaObject(numbers);
+// newNumbers.x = 15;
+// console.log(newNumbers);
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// ES6  clonlash  Object.assign() metodi bu OSON YO'LI!!!!!!
+// const numbers = {
+//     x: 10,
+//     y: 5,
+// };
+// const newNumbers = Object.assign({}, numbers);
+// newNumbers.x = 15;
+// console.log(newNumbers); //yani Object.assign() metodi bilan es6 da clonlash juda oson
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ES6  clonlash  Object.assign() metodi bilan ikkita objectni birlashtirish yani bir biriga qo'shish
+// const numbers = {
+//     x: 10,
+//     y: 5,
+// };
+// const addNumber = {
+//     z: 15,
+// };
+// const allNumbers = Object.assign(numbers, addNumber);
+// console.log(allNumbers);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Arraylarda clonlash slice metodi
+// const arr = [1, 2, 3];
+// const numbers = arr.slice();
+// console.log(numbers);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///slice metodi bilan arrayni ichidan tanlab olib nusxalash
+///////////// 0  1  2  3
+const arr2 = [1, 2, 3, 4];
+/////////////////////////// 0  3 yani 2 ni o'zini alohida nusxalab oldi
+const numbers2 = arr2.slice(1, 2);
+console.log(numbers2);
--- 51.chi dars JSON Chuqur clonlash/script51.js
+++ 51.chi dars JSON Chuqur clonlash/script51.js
@@ -1,1 +1,44 @@
 "use strict";
+////JSON (JavaScript Object Notation)
+
+////server hech qachon oddiy objectni qabul qilmaydi yani tushunmeydi
+////serverga object yuborilsa har doim json formatda yuborilishi kerak
+
+////objectni json filega o'girish
+////JSON.stringify metodi
+// const car = {
+//     name: "bmw",
+//     color: "red",
+// };
+// const objToJson = JSON.stringify(car)//yani stringify metodi objectni json failga o'giradi object qiymatlari doim ikkita qo'shtirnoq ichida bo'ladi>>>{"name":"bmw","color":"red"}<<<shu ko'rinish json filega ishora qiladi server esa doim va faqat shu ikkita qo'shtornoq ichidagi malumotlarni qabul qiladi
+// console.log(objToJson);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////serverdan keladigan json formatdagi malumotni js tushadigan formatga o'girish
+// ////jsonni object filega o'girish
+// ////JSON.parse metodi
+// const carr = {
+//     name: "bmw",
+//     color: "red",
+// };
+// const objToJsonn = JSON.stringify(carr); //yani bu holatda carr nomli o'zgaruvchi yaratildi va objToJsonn nomli yangi o'zgaruvchi yaratib JSON objecti stringify metodi bilan chaqirildi bu joyda stringify metodini parametriga carr objecti qo'yildi va stringify metodi carr objectini js filedan server tushunadigan ikkita qo'shtirnoqli>>>{"name":"bmw","color":"red"}objectga aylantirdi yani stringify metodi bilan serverga malumotlarni json format ko'rinishida jo'natish mumkun chunki server faqat json fileni yani ikkita qo'shtirnoq bilan yozilgan filelarni taniydi
+
+// const jsonToObject = JSON.parse(objToJsonn); //bu holatda jsonToObject nomli o'zgaruvchi yaratilib unga JSON js objectini parse metodi bilan chaqirildi va parametriga jsonga aylantirilib serverga ketgan objToJsonn o'zgaruvchsi qo'yildi yani server faqat json qabul qiladi va json qaytaradi bu holatda parse metodi json fileni yana js tushunadigan filega aylantirdi (qo'shtirnoqlariga etibor ber>>>{ name: 'bmw', color: 'red' }//yanni serverdan qandaydur malumotlar kelganda albatta parse metodi bilan js filega yani objectga aylantirilishi kerak
+// console.log(objToJsonn);
+// console.log(jsonToObject);
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////Chuqur clonlash yani objectni ichidagi objectni clonlash JSON parse va JSON stringify metodlari bilan
+const carr = {
+    name: "bmw",
+    color: "red",
+    extra: {
+        //yani carr objectini icxhidagi local object OOP!
+        isAirbag: false,
+        tires: 5,
+    },
+};
+const clone = JSON.parse(JSON.stringify(carr)); //yani bu holatda clone deb nomlangan yangi o'zgaruvchi yaratib JSON objectini parse metodini chaqirdik bu parse metodi bu holatda ichma ich objectdan iborat carr objectni butunlay nusxalaydi parse metodini parametrida esa stringify metodi chaqirildi(shu joyiga izoh yo'q)???
+clone.extra.tires = 10;
+console.log(clone); //{ name: 'bmw', color: 'red', extra: { isAirbag: false, tires: 10 } }//yani objectni ichidagi local objectham clonlandi
+console.log(carr);
